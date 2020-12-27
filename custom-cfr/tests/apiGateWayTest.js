b4271fb9b5966e56bbf760b20343bbbfa22eb5fb
Christopher Hill <chrishill9@gmail.com>
2020-12-27 18:00:01 -0500

    API Gateway creation

--- custom-cfr/tests/apiGateWayTest.js
+++ custom-cfr/tests/apiGateWayTest.js
@@ -2,7 +2,10 @@ const g = require('../utils/apiGateway');
 const AWS = require('aws-sdk');
 
 (async function(){
-    var credentials = new AWS.SharedIniFileCredentials({profile: 'pasley_hill'});
+    
+    const credentials = new AWS.SharedIniFileCredentials({profile: 'pasley_hill'});
     AWS.config.credentials = credentials;
+
     await g.createApiGateway(AWS);
+
 })();
--- custom-cfr/utils/apiGateway.js
+++ custom-cfr/utils/apiGateway.js
@@ -3,20 +3,35 @@ async function createApiGateway(AWS){
     const apig = new AWS.APIGateway({apiVersion: '2015/07/09',
     profile: 'pasley_hill'});
     const params = {
-        name: "Simple PetStore (node.js SDK)",
+        name: "Weather API",
         binaryMediaTypes: [
             '*'
         ],
-        description: "Demo API created using the AWS SDK for node.js",
+        description: "Demo API for weather in Tulsa OK",
         version: "0.00.001"
     };
     const restResponse = await apig.createRestApi(params).promise();
     const resource = await apig.getResources({ 
-        restApiId:  restResponse.restApiId
-    });
-    
+        restApiId:  restResponse.id
+    }).promise();
+    const parent = resource.items.find(r => r.path === "/");
+    console.log(resource);
+    const resourceCreate = await apig.createResource({
+        restApiId: restResponse.id,
+        parentId: parent.id,
+        pathPart: 'weather'
+    }).promise();
 
-    return response;
+    const methodResponse = await apig.putMethod({
+        restApiId: restResponse.id,
+        parentId: parent.id,
+        httpMethod: 'GET',
+        authorizationType: 'NONE'
+    }).promise();
+
+    console.log(methodResponse);
+
+    //return response;
 }
 
 module.exports = { createApiGateway };
