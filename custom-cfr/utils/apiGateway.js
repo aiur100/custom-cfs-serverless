e1dee4c08b12aac5ec7a578cbc18873e05f857da
Christopher Hill <chrishill9@gmail.com>
2020-12-27 21:20:37 -0500

    Testing API Gateway

--- custom-cfr/utils/apiGateway.js
+++ custom-cfr/utils/apiGateway.js
@@ -1,7 +1,12 @@
 async function createApiGateway(AWS){
+
     AWS.config.region = 'us-east-1';
-    const apig = new AWS.APIGateway({apiVersion: '2015/07/09',
-    profile: 'pasley_hill'});
+
+    const apig = new AWS.APIGateway({
+        apiVersion: '2015/07/09',
+        profile: 'pasley_hill'
+    });
+
     const params = {
         name: "Weather API",
         binaryMediaTypes: [
@@ -10,21 +15,26 @@ async function createApiGateway(AWS){
         description: "Demo API for weather in Tulsa OK",
         version: "0.00.001"
     };
+
     const restResponse = await apig.createRestApi(params).promise();
+
     const resource = await apig.getResources({ 
         restApiId:  restResponse.id
     }).promise();
+
     const parent = resource.items.find(r => r.path === "/");
     console.log(resource);
+
     const resourceCreate = await apig.createResource({
         restApiId: restResponse.id,
         parentId: parent.id,
         pathPart: 'weather'
     }).promise();
+    console.log(resourceCreate);
 
     const methodResponse = await apig.putMethod({
         restApiId: restResponse.id,
-        parentId: parent.id,
+        resourceId: parent.id,
         httpMethod: 'GET',
         authorizationType: 'NONE'
     }).promise();
